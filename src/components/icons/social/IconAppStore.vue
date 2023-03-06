055bdddf46d9b76d31e582dcdfff1d0df8c957c6
Danil Zalizchuk <danilmail0110@gmail.com>
2023-03-06 23:58:03 +0200

    delete unused icons

--- src/components/icons/social/IconAppStore.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <img :src="appStore" alt="Download from App Store icon" />
-</template>
-
-<script setup>
-import appStore from '@/assets/icons/app-store.png';
-</script>
--- src/components/icons/social/IconGooglePlay.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <img :src="googlePlay" alt="Download from Google Play icon" />
-</template>
-
-<script setup>
-import googlePlay from '@/assets/icons/google-play.png';
-</script>
